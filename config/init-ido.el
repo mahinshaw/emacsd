7ffa3162ccc98107597f5c9427e29c18fa177a88
mahinshaw <mahinshaw@gmail.com>
2014-11-07 18:24:28 -0500

    Web and ido changes

--- config/init-ido.el
+++ config/init-ido.el
@@ -3,40 +3,32 @@
 (progn
   (require-package 'ido-ubiquitous)
   (require-package 'flx-ido)
+  (require-package 'smex)
+
+  (require 'ido-ubiquitous)
   (require 'flx-ido)
   ;; Turn on ido-mode
   (ido-mode t)
-  (setq ido-everywhere t)
-  ;; turn on ido-flx
+  (ido-ubiquitous-mode t)
+  ;; Better Fuzzy finder
   (flx-ido-mode 1)
 
-  ;; Fuzzy finder
-  (setq ido-enable-flex-matching t)
-  (setq ido-use-faces nil)
-  ;; AutoCompletions for C-x C-f (kinda cool, kinda jarring).
-  (setq ido-use-filename-at-point 'guess)
-  ;; Create new buffers by default when a buffer of that name does not exist.
-  (setq ido-create-new-buffer 'always)
-  ;; set order of files displayed in minibuffers
-  ;; I dont have a preference at this point, but it could be useful down the road.
-  ;;(setq ido-file-extension-order '(".clj" ".txt"))
-
-  ;; Setting to true means that case is ignored.
-  ;;(setq ido-case-fold t)
-
-  ;; turn on extension ignore
-  (setq ido-ignore-extensions t)
-  (setq completion-ignored-extensions '(".obj" ".pdb" ".exe" ".dll"))
-
   ;; Really use ido everywhere.
-  (require 'ido-ubiquitous)
   (setq ido-ubiquitous-mode 1)
+  (setq ido-enable-prefix nil
+        ido-enable-flex-matching t
+        ido-use-faces nil
+        ido-use-filename-at-point 'guess
+        ido-max-prospects 10
+        ido-create-new-buffer 'always
+        ido-save-directory-list-file (expand-file-name "ido.hist" emacsd-cache-directory)
+        ido-ignore-extensions t
+        completion-ignored-extensions '(".obj" ".pdb" ".exe" ".dll"))
 
   ;; Use smex - It's here because it is attached to Ido
-  (require-package 'smex)
   (require 'smex)
+  (setq smex-save-file (expand-file-name ".smex-items" emacsd-cache-directory))
   (global-set-key (kbd "M-x") 'smex)
-  (global-set-key (kbd "M-X") 'smex-major-mode-command)
-  )
+  (global-set-key (kbd "M-X") 'smex-major-mode-command))
 
 (provide 'init-ido)
---
+++ config/init-web.el
@@ -0,0 +1,25 @@
+;; Web-mode related configurations.
+(require-package 'web-mode)
+(require 'web-mode)
+
+;; add file types that will use web mode.
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+;; web mode pairing clobbers smartparens
+(setq web-mode-enable-auto-pairing nil)
+
+(defun my-web-mode-hook ()
+  "Hook up with `web-mode'."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+(provide 'init-web)
